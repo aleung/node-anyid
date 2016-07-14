487188c7718c6f4c31d3dccc610e109776e2fb6c
Leo Liang <leoliang@gmail.com>
2016-07-14 16:28:39 +0800

    no message

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,12 @@
+import * as assert from 'assert';
 import * as _ from 'lodash';
+
+// for debug
+function logBin(n: number) {
+  const bin = _.padStart((n >>> 0).toString(2), 32, '0');
+  const hex = _.padStart((n >>> 0).toString(16), 8, '0');
+  console.log(bin, hex);
+}
 
 /*
  * 0B0000
@@ -6,16 +14,27 @@ import * as _ from 'lodash';
  * 0B0011
  * ......
  */
-const BIT_MASKS = _.range(0, 31).map(i => 0xFFFFFFFF >>> i);
+const BIT_MASKS = _.range(0, 31).map(i => ~ (0xFFFFFFFF << i));
 
 /**
  * Input a bit stream which is align right in a Buffer.
  * Normalize it to Buffer whose length is multiple of 4 and fill unused bits at the left with 0.
  */
-function normalize(buf: Buffer, bits: number): Buffer {
-  const length = Math.ceil(bits / 32) * 4;
-  const target = Buffer.allocUnsafe(length);
-  buf.copy(target, 0, buf.length - length, length);
+export function normalize(source: Buffer, bits: number): Buffer {
+  const sourceSignificantBytes = Math.ceil(bits / 8);
+  assert(source.length >= sourceSignificantBytes, 'Source buffer has no enought bits');
+  const targetLength = Math.ceil(bits / 32) * 4;
+  const target = Buffer.alloc(targetLength);
+  source.copy(target, targetLength - sourceSignificantBytes,
+    source.length - sourceSignificantBytes);
+  // fill usused bits at the left with 0
+  const unusedBits = targetLength * 8 - bits;
+  if (unusedBits > 0) {
+    let highestBytes = target.readUInt32BE(0);
+    highestBytes = highestBytes & BIT_MASKS[32 - unusedBits];
+    target.writeUInt32BE(highestBytes, 0);
+  }
+
   return target;
 }
 
@@ -26,15 +45,16 @@ function normalize(buf: Buffer, bits: number): Buffer {
  */
 export function concatBits(buf1: Buffer, bits1: number, buf2: Buffer, bits2: number): Buffer {
   const buf = Buffer.allocUnsafe(Math.ceil((bits1 + bits2) / 32) * 4);
-  const buf2SignificantBytes = Math.ceil(bits2 / 32) * 4;
+  const buf2SignificantBytes = Math.ceil(bits2 / 8);
   let targetOffset = buf.length - buf2SignificantBytes;
 
   // copy buf2 into output buf, right align
-  buf2.copy(buf, targetOffset, buf2.length - buf2SignificantBytes, buf2SignificantBytes);
+  buf2.copy(buf, targetOffset, buf2.length - buf2SignificantBytes);
 
+  targetOffset = buf.length - Math.ceil(bits2 / 32) * 4;
   const filledBitsInTarget = bits2 % 32;
-  if (filledBitsInTarget > 0) {
-    targetOffset++;
+  if (filledBitsInTarget === 0) {
+    targetOffset -= 4;
   }
 
   const sourceBuf = normalize(buf1, bits1);
@@ -42,64 +62,26 @@ export function concatBits(buf1: Buffer, bits1: number, buf2: Buffer, bits2: num
 
   let uncopiedBytes = sourceBuf.length;
   while (uncopiedBytes > 0) {
-
     sourceReadOffset -= 4;
-    const source4bytes = buf1.readUInt32LE(sourceReadOffset);
+    const source4bytes = sourceBuf.readUInt32BE(sourceReadOffset);
 
-    targetOffset -= 4;
-    let target4bytes = buf.readUInt32LE(targetOffset);
+    let target4bytes = buf.readUInt32BE(targetOffset);
 
     target4bytes =
-      target4bytes & BIT_MASKS[filledBitsInTarget] |
-      source4bytes << filledBitsInTarget;
-    buf.writeUInt32LE(target4bytes, targetOffset);
+      (target4bytes & BIT_MASKS[filledBitsInTarget] |
+      source4bytes << filledBitsInTarget)
+      >>> 0;
+    buf.writeUInt32BE(target4bytes, targetOffset);
 
-    target4bytes = source4bytes >>> ( 32 - filledBitsInTarget);
-    const bytesToWrite = targetOffset > 4 ? 4 : targetOffset;
-    buf.writeUIntLE(target4bytes, targetOffset - bytesToWrite, bytesToWrite);
+    if (filledBitsInTarget > 0) {
+      target4bytes = source4bytes >>> ( 32 - filledBitsInTarget);
+      const bytesToWrite = targetOffset > 4 ? 4 : targetOffset;
+      buf.writeUIntBE(target4bytes, targetOffset - bytesToWrite, bytesToWrite);
+    }
 
+    targetOffset -= 4;
     uncopiedBytes -= 4;
   }
 
   return buf;
 }
-
-/*
-export function concatBits_old(buf1: Buffer, bits1: number, buf2: Buffer, bits2: number): Buffer {
-  const buf = Buffer.allocUnsafe(Math.ceil((bits1 + bits2) / 32) * 4);
-  const buf2SignificantBytes = Math.ceil(bits2 / 32) * 4;
-  let targetOffset = buf.length - buf2SignificantBytes;
-
-  // copy buf2 into output buf, right align
-  buf2.copy(buf, targetOffset, buf2.length - buf2SignificantBytes, buf2SignificantBytes);
-
-  const filledBitsInTarget = bits2 % 32;
-  if (filledBitsInTarget > 0) {
-    targetOffset++;
-  }
-  let buf1UncopiedBits = bits1;
-  let buf1ReadOffset = buf1.length;
-
-  while (buf1UncopiedBits > 0) {
-    const bytesToCopy =
-      buf1UncopiedBits > 32 ?
-        (buf1UncopiedBits > 16 ? 2 : 1)
-        : 4;
-    buf1ReadOffset -= bytesToCopy;
-    const sourceBytes = buf1.readUIntLE(buf1ReadOffset, bytesToCopy);
-    buf1UncopiedBits -= bytesToCopy * 8;
-    targetOffset -= bytesToCopy;
-    let targetBytes = buf.readUIntLE(targetOffset, bytesToCopy);
-
-    targetBytes =
-      targetBytes & BIT_MASKS[filledBitsInTarget] |
-      sourceBytes << filledBitsInTarget & ~ (BIT_MASKS[filledBitsInTarget] << bytesToCopy * 8);
-    buf.writeUIntLE(targetBytes, targetOffset, bytesToCopy);
-
-    targetBytes = sourceBytes >>> ( bytesToCopy * 8 - filledBitsInTarget);
-    buf.writeUIntLE(targetBytes, targetOffset - bytesToCopy, bytesToCopy);
-  }
-
-  return buf;
-}
-*/
